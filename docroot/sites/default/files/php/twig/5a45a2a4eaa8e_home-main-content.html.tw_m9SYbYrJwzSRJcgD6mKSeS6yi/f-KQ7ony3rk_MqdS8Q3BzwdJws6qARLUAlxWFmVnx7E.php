8d480cc93ba4972063e2e17f9092e3e94fbf7b16
Prodru01 <test@cineca.it>
2017-12-29 18:31:11 +0000

    Initial commit

---
+++ docroot/sites/default/files/php/twig/5a45a2a4eaa8e_home-main-content.html.tw_m9SYbYrJwzSRJcgD6mKSeS6yi/f-KQ7ony3rk_MqdS8Q3BzwdJws6qARLUAlxWFmVnx7E.php
@@ -0,0 +1,78 @@
+<?php
+
+/* modules/custom/dddlx_home/templates/home-main-content.html.twig */
+class __TwigTemplate_95d386f742e336e1437c8582c63ba98ba8f3af95e1f83c935cba89475bc2ccc3 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array();
+        $filters = array("t" => 1);
+        $functions = array();
+
+        try {
+            $this->env->getExtension('Twig_Extension_Sandbox')->checkSecurity(
+                array(),
+                array('t'),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 1
+        echo "<h2 class=\"hero__text\">";
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->renderVar(t("The main Drupal developer-oriented")));
+        echo " <span>";
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->renderVar(t("international event in Europe.")));
+        echo "</span></h2>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "modules/custom/dddlx_home/templates/home-main-content.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  43 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("", "modules/custom/dddlx_home/templates/home-main-content.html.twig", "/Users/hernani/Sites/ddd_sevilla/dddlisbon/docroot/modules/custom/dddlx_home/templates/home-main-content.html.twig");
+    }
+}
