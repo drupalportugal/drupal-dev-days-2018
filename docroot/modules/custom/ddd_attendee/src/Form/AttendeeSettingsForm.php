630cdf9c688e25177d1fd37eeb350246bda24bd5
fmfpereira@gmail.com <fmfpereira@gmail.com>
2018-01-07 18:41:04 +0000

    Add Attendee settings form

--- docroot/modules/custom/ddd_attendee/src/Form/AttendeeSettingsForm.php
+++ docroot/modules/custom/ddd_attendee/src/Form/AttendeeSettingsForm.php
@@ -2,52 +2,61 @@
 
 namespace Drupal\ddd_attendee\Form;
 
-use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
 
 /**
  * Class AttendeeSettingsForm.
- *
- * @ingroup ddd_attendee
  */
-class AttendeeSettingsForm extends FormBase {
+class AttendeeSettingsForm extends ConfigFormBase {
 
   /**
-   * Returns a unique string identifying the form.
-   *
-   * @return string
-   *   The unique string identifying the form.
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return [
+      'ddd_attendee.settings',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
    */
   public function getFormId() {
-    return 'attendee_settings';
+    return 'attendee_settings_form';
   }
 
   /**
-   * Form submission handler.
-   *
-   * @param array $form
-   *   An associative array containing the structure of the form.
-   * @param \Drupal\Core\Form\FormStateInterface $form_state
-   *   The current state of the form.
-   */
-  public function submitForm(array &$form, FormStateInterface $form_state) {
-    // Empty implementation of the abstract submit class.
-  }
-
-  /**
-   * Defines the settings form for Attendee entities.
-   *
-   * @param array $form
-   *   An associative array containing the structure of the form.
-   * @param \Drupal\Core\Form\FormStateInterface $form_state
-   *   The current state of the form.
-   *
-   * @return array
-   *   Form definition array.
+   * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $form['attendee_settings']['#markup'] = 'Settings form for Attendee entities. Manage field settings here.';
-    return $form;
+    $config = $this->config('ddd_attendee.settings');
+    $form['webhook_security_token'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Webhook security token.'),
+      '#description' => $this->t('The ti.to webhook security token. Leave empty if you want to allow all requests.'),
+      '#maxlength' => 64,
+      '#size' => 64,
+      '#default_value' => $config->get('webhook_security_token'),
+    ];
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    parent::validateForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    parent::submitForm($form, $form_state);
+    $this->config('ddd_attendee.settings')
+      ->set('webhook_security_token', $form_state->getValue('webhook_security_token'))
+      ->save();
   }
 
 }
