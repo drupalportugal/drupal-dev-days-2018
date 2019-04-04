59921ddbd3f6e2f26004d18b0a96788f7ea11a24
Hernani Borges de Freitas <hernani@Hernanis-MacBook-Air.local>
2019-04-04 22:28:41 +0100

    Static version of the website crated with tome.

--- docroot/modules/custom/ddd_attendee/src/Exception/TitoWebhookException.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Drupal\ddd_attendee\Exception;
-
-/**
- * Defines an exception thrown when an invalid webhook request is found.
- */
-class TitoWebhookException extends \Exception {}
